a744fd599e5bc721ea139da0c48af628c9efeb8a
Jeehvi <javi.gil.navarro@outlook.es>
2019-05-13 19:45:55 +0200

    trabajando en el menu de la aplicacion

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, OnInit, AfterViewInit, ChangeDetectorRef } from "@angular/core";
 import { RadSideDrawerComponent } from "nativescript-ui-sidedrawer/angular";
 import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
+import {Page} from "tns-core-modules/ui/page";
 
 @Component({
     moduleId: module.id,
@@ -11,7 +12,7 @@ import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
 export class MenuComponent implements AfterViewInit, OnInit {
     private _mainContentText: string;
 
-    constructor(private _changeDetectionRef: ChangeDetectorRef) {
+    constructor(private _changeDetectionRef: ChangeDetectorRef, private page:Page) {
     }
 
     @ViewChild(RadSideDrawerComponent) public drawerComponent: RadSideDrawerComponent;
@@ -23,6 +24,7 @@ export class MenuComponent implements AfterViewInit, OnInit {
     }
 
     ngOnInit() {
+        this.page.actionBarHidden =true;
         this.mainContentText = "SideDrawer for NativeScript can be easily setup in the HTML definition of your page by defining tkDrawerContent and tkMainContent. The component has a default transition and position and also exposes notifications related to changes in its state. Swipe from left to open side drawer.";
     }
 
